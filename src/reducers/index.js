7570576b153b678ae49c71553e340ac0511239a7
Jonathan Jalouzot <jonathan.jalouzot@gmail.com>
2017-07-11 15:48:56 +0200

    feat(test): add jest test

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import state from './state';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  state
 });
 
 export default rootReducer;
