4b34deb46c7f430b4fb7341fa5814c243e9fc678
Kamal Farsaoui <elfa.kamal@gmail.com>
2017-11-06 18:57:23 +0100

    better build

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -24,6 +24,10 @@ export class Home extends React.Component {
 
   componentDidMount() {
     this.props.loadCourses();
+
+    fetch('https://the-wilson-post.appspot.com/api/posts').then(posts => {
+      console.log(posts);
+    });
   }
 
   render() {
