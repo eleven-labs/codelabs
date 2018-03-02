684501ae4001fc8d9eba5750a5f1d0b7d5c583e8
Martin Cadoux <martin.cadoux@me.com>
2018-03-02 19:38:29 +0100

    Escape missing quotes

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -11,16 +11,17 @@ class Splash extends Component {
     return (
       <div className="splash">
         <div className="splash__container container">
-          <img className="splash__icon" src={flask} alt="Code Labs" />
+          <img className="splash__icon" src={flask} alt="" />
           <div className="splash__text">
             <h2 className="splash__title">
-              Bienvenue sur Eleven Code Labs
+              Bienvenue sur Eleven Codelabs
             </h2>
             <p className="splash__description">
               Cette plateforme ? C&rsquo;est votre nouvelle source de tutoriels,
-              orient&eacute;s d&eacute;veloppement d'applicatifs web et mobile et ma&icirc;trise d&rsquo;outils DevOps !
+              orient&eacute;s d&eacute;veloppement d&rsquo;applicatifs web et mobile
+              et ma&icirc;trise d&rsquo;outils DevOps !
               Vous pourrez y suivre &agrave; votre rythme des exercices complets,
-              allant du d&eacute;veloppement d'applications
+              allant du d&eacute;veloppement d&rsquo;applications
               &agrave; des cas pratiques vous faisant d&eacute;couvrir un nouvel outil.
               Garez tranquillement votre vaisseau, enfilez votre combinaison,
               et rejoignez-nous dans le laboratoire !
