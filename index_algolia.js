2d31987e704c35c5e629afd50c923d22ac38a98e
Kamal Farsaoui <elfa.kamal@gmail.com>
2018-04-01 21:45:46 +0200

    remove package-lock & fix some eslint issues

--- index_algolia.js
+++ index_algolia.js
@@ -3,31 +3,28 @@ const algoliasearch = require('algoliasearch');
 const data = require('./_posts/codelabs/index.json');
 
 const client = algoliasearch(process.env.APP_ID, process.env.ADMIN_API_KEY);
-
 const index = client.initIndex('codelabs');
 
 index.clearIndex((err, content) => {
   if (err) {
     console.error(content);
   }
-  
-  index.addObjects(data, (err, content) => {
-    if (err) {
-      console.error(content);
+
+  index.addObjects(data, (addObjectsErr, addObjectsContent) => {
+    if (addObjectsErr) {
+      console.error(addObjectsContent);
     }
-    
+
     index.setSettings({
       searchableAttributes: [
         'title',
         'tags',
         'excerpt',
       ],
-    }, (err, content) => {
-      if (err) {
-        console.error(content);
+    }, (setSettingsErr, setSettingsContent) => {
+      if (setSettingsErr) {
+        console.error(setSettingsContent);
       }
     });
   });
 });
-
-
